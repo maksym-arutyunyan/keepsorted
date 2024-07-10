5621f895e483a84759974f5c0235d3a57eaa9f4b
Maksym Arutyunyan <m.arutyunyan+github@gmail.com>
2024-07-10 09:34:31 +0200

    add ignored tests

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,7 @@ TEST_DIR="./tests"
 # Define color codes
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 # Build the Rust project
@@ -27,6 +28,12 @@ for src_file in "$TEST_DIR"/*_in*; do
     base_name=$(basename "$src_file" "_in${src_file##*_in}")
     extension="${src_file##*_in}"
     expected_file="$TEST_DIR/${base_name}_out${extension}"
+
+    # Check if the test is disabled
+    if [[ "$(basename "$src_file")" == _* ]]; then
+        echo -e "test $base_name ... ${YELLOW}ignored${NC}"
+        continue
+    fi
 
     # Check if the expected file exists
     if [ ! -f "$expected_file" ]; then
