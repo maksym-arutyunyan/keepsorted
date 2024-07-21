e665a0cffbf2740eb3b6cd2a91b07bebeae965dd
Maksym Arutyunyan <m.arutyunyan+github@gmail.com>
2024-07-21 19:39:45 +0200

    add generic tests

--- tests/generic.rs
+++ tests/generic.rs
@@ -4,21 +4,6 @@ mod common;
 use keepsorted::SortStrategy::Generic;
 
 #[test]
-fn generic_no_sorting_comment() {
-    test_inner!(
-        Generic,
-        r#"
-b
-a
-        "#,
-        r#"
-b
-a
-        "#
-    );
-}
-
-#[test]
 fn generic_simple_block() {
     test_inner!(
         Generic,
@@ -36,24 +21,116 @@ b
 }
 
 #[test]
-fn generic_blocks_divided_by_newline() {
+fn generic_blocks_with_newline() {
     test_inner!(
         Generic,
         r#"
 # Keep sorted.
-d
-c
+y
+x
 
 b
 a
         "#,
         r#"
 # Keep sorted.
-c
-d
+x
+y
 
 b
 a
+        "#
+    );
+}
+
+#[test]
+fn generic_several_blocks() {
+    test_inner!(
+        Generic,
+        r#"
+# Keep sorted.
+y
+x
+
+# Keep sorted.
+b
+a
+        "#,
+        r#"
+# Keep sorted.
+x
+y
+
+# Keep sorted.
+a
+b
+        "#
+    );
+}
+
+#[test]
+fn generic_block_with_multi_line_comment() {
+    test_inner!(
+        Generic,
+        r#"
+# Keep sorted.
+y
+# Some multi-line comment
+# for the line below.
+x
+b
+a
+        "#,
+        r#"
+# Keep sorted.
+a
+b
+# Some multi-line comment
+# for the line below.
+x
+y
+        "#
+    );
+}
+
+#[test]
+fn generic_block_with_trailing_comment() {
+    test_inner!(
+        Generic,
+        r#"
+# Keep sorted.
+b
+a
+# Some multi-line comment
+# trailing comment.
+        "#,
+        r#"
+# Keep sorted.
+a
+b
+# Some multi-line comment
+# trailing comment.
+        "#
+    );
+}
+
+#[test]
+fn generic_block_with_inline_comment() {
+    test_inner!(
+        Generic,
+        r#"
+# Keep sorted.
+y
+x  # Some in-line comment.
+b
+a
+        "#,
+        r#"
+# Keep sorted.
+a
+b
+x  # Some in-line comment.
+y
         "#
     );
 }
