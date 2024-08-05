29b4308965b3f740ef3e97eadb65c1cf0aa02331
Maksym Arutyunyan <m.arutyunyan+github@gmail.com>
2024-08-05 22:07:27 +0200

    cleanup process_file

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,33 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, BufWriter, Write};
 use std::path::Path;
 
 pub mod strategies;
 
 pub fn process_file(path: &Path) -> io::Result<()> {
-    let mut content = std::fs::read_to_string(path)?;
+    let mut content = fs::read_to_string(path)?;
     let ends_with_newline = content.ends_with('\n');
     if !ends_with_newline {
-        // Add trailing '\n' so all the lines have it.
         content.push('\n');
     }
-    let lines: Vec<&str> = content.split_inclusive('\n').collect();
 
+    let lines: Vec<&str> = content.split_inclusive('\n').collect();
     let output_lines = process_lines(classify(path), lines)?;
 
-    let n = output_lines.len();
-    let output_file = File::create(path)?;
-    let mut writer = BufWriter::new(output_file);
+    let mut writer = BufWriter::new(File::create(path)?);
     for (i, line) in output_lines.iter().enumerate() {
-        if i + 1 == n && !ends_with_newline {
-            // Remove trailing '\n' since there were none in the source.
-            write!(writer, "{}", line.trim_end_matches('\n'))?;
-        } else {
-            write!(writer, "{}", line)?;
-        }
+        write!(
+            writer,
+            "{}",
+            if i + 1 == output_lines.len() && !ends_with_newline {
+                line.trim_end_matches('\n')
+            } else {
+                line
+            }
+        )?;
     }
 
-    writer.flush()?;
-    Ok(())
+    writer.flush()
 }
 
 pub enum Strategy {
