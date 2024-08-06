d1eea0e5963b896c4cce8f5dfb8757e4d6da5a18
Maksym Arutyunyan <m.arutyunyan+github@gmail.com>
2024-08-06 08:31:25 +0200

    .

--- src/strategies/generic.rs
+++ src/strategies/generic.rs
@@ -1,5 +1,35 @@
+use regex::Regex;
 use std::io;
 
-pub(crate) fn process<'a>(_lines: Vec<String>) -> io::Result<Vec<String>> {
-    Ok(vec![])
+const STRATEGY: crate::Strategy = crate::Strategy::Generic;
+
+pub(crate) fn process(lines: Vec<String>) -> io::Result<Vec<String>> {
+    let re = Regex::new(r"^\s*#\s*Keep\s*sorted\.\s*$")
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    let mut output_lines = Vec::new();
+    let mut block = Vec::new();
+    let mut is_sorting_block = false;
+
+    for line in lines {
+        if re.is_match(&line) {
+            is_sorting_block = true;
+            output_lines.push(line);
+        } else if is_sorting_block && line.trim().is_empty() {
+            is_sorting_block = false;
+            //sort(&mut block, STRATEGY);
+            output_lines.append(&mut block);
+            output_lines.push(line);
+        } else if is_sorting_block {
+            block.push(line);
+        } else {
+            output_lines.push(line);
+        }
+    }
+
+    if is_sorting_block {
+        //sort(&mut block, STRATEGY);
+        output_lines.append(&mut block);
+    }
+
+    Ok(output_lines)
 }
