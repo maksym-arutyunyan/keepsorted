40e082b969102007a932cb1b668797f572f23a02
Maksym Arutyunyan <m.arutyunyan+github@gmail.com>
2024-09-01 10:15:47 +0200

    cleanup

--- research/derive_order/use_macros/src/lib.rs
+++ research/derive_order/use_macros/src/lib.rs
@@ -1,3 +1,5 @@
+// This test file checks if the order of Rust derive traits matters.
+// Since the keepsorted tool could reorder these traits in CI, we must ignore this file.
 // keepsorted: ignore file
 
 #[cfg(test)]
@@ -14,7 +16,7 @@ mod tests {
 
     #[test]
     fn test_first_second() {
-        #[derive(First, Second)] // <- This is expected to work.
+        #[derive(First, Second)] // <- This order should work.
         struct MyStruct;
 
         assert!(MyStruct::check_first_is_implemented());
@@ -22,7 +24,7 @@ mod tests {
 
     #[test]
     fn test_second_first() {
-        #[derive(Second, First)] // <- Seems the order is not important.
+        #[derive(Second, First)] // <- This order might not work.
         struct MyStruct;
 
         assert!(MyStruct::check_first_is_implemented());
