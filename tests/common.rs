0889b0423a124594a7d8a0b2353eb847de1e4592
Rasmus <rasmus_vermeulen@tutanota.com>
2025-06-07 23:49:19 +0200

    tests/common: use optional TraitGroups

--- tests/common.rs
+++ tests/common.rs
@@ -2,9 +2,13 @@ use keepsorted::{process_lines, Strategy, TraitGroups};
 use std::io::{self};
 
 // Helper function to hide text-lines conversion.
-pub fn process_input(strategy: Strategy, text: &str, groups: &TraitGroups) -> io::Result<String> {
+pub fn process_input(
+    strategy: Strategy,
+    text: &str,
+    groups: Option<&TraitGroups>,
+) -> io::Result<String> {
     let lines: Vec<_> = text.lines().map(|line| format!("{}\n", line)).collect();
-    let mut processed_lines = process_lines(strategy, lines, Some(groups))?;
+    let mut processed_lines = process_lines(strategy, lines, groups)?;
     if let Some(last) = processed_lines.last_mut() {
         last.truncate(last.trim_end_matches('\n').len());
     }
@@ -17,9 +21,8 @@ macro_rules! test_inner {
     ($strategy:expr, $input:expr, $expected:expr) => {{
         let strategy = $strategy;
         let input = $input;
-        let groups = Default::default();
         let expected = $expected;
-        let result = common::process_input(strategy, input, &groups).unwrap();
+        let result = common::process_input(strategy, input, None).unwrap();
         assert!(
             result == expected,
             "Expected: {}\nActual: {}",
@@ -32,7 +35,7 @@ macro_rules! test_inner {
         let input = $input;
         let groups = $groups;
         let expected = $expected;
-        let result = common::process_input(strategy, input, groups).unwrap();
+        let result = common::process_input(strategy, input, Some(&groups)).unwrap();
         assert!(
             result == expected,
             "Expected: {}\nActual: {}",
