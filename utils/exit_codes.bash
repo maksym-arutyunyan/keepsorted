469bb8987c5c1cf6280dd888ea85d20ce12606ac
Maksym Arutyunyan <107496615+maksym-arutyunyan@users.noreply.github.com>
2025-07-25 14:56:01 +0200

    fix(tests): export exit codes in helpers

--- utils/exit_codes.bash
+++ utils/exit_codes.bash
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 # Common exit codes matching README documentation
 
-readonly EXIT_SUCCESS=0
-readonly EXIT_SYNTAX_ERROR=1
-readonly EXIT_USAGE_ERROR=2
-readonly EXIT_RUNTIME_ERROR=3
-readonly EXIT_CHECK_FAILED=4
+declare -xr EXIT_SUCCESS=0
+declare -xr EXIT_SYNTAX_ERROR=1
+declare -xr EXIT_USAGE_ERROR=2
+declare -xr EXIT_RUNTIME_ERROR=3
+declare -xr EXIT_CHECK_FAILED=4
