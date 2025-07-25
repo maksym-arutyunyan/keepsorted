87c7db930384426d043082ba954df4711c0de60c
Maksym Arutyunyan <107496615+maksym-arutyunyan@users.noreply.github.com>
2025-07-25 18:54:07 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,8 @@ fn about() -> String {
 }
 
 fn long_about() -> String {
-    "A tool for sorting blocks of lines in code files.\n\
+    "\n\
+A tool for sorting blocks of lines in code files.\n\
 Sort lists inside '# Keep sorted' blocks. Generic and Bazel files require the comment. \
 Cargo.toml, .gitignore and CODEOWNERS are sorted automatically. \
 Skip sorting with '# keepsorted: ignore file' or '# keepsorted: ignore block'. \
